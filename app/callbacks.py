3c74ab8f921166e0c36c23e9719684bbaf84b9a7
boorav4ik <67604789+boorav4ik@users.noreply.github.com>
2021-11-08 10:47:38 +0300

    feat(callbacks): переделал на clientside_callback

--- app/callbacks.py
+++ app/callbacks.py
@@ -5,6 +5,7 @@ from components.tools_control import render_menu_items
 from components.tools import analitic_tools
 from components.data_table import data_table_niche
 
+
 @app.callback(Output("tools_menu", "children"),
               Output("tools_menu", "value"),
               Input("tools_menu_selector", "value"),
@@ -12,11 +13,13 @@ from components.data_table import data_table_niche
 def change_tools_menu_select(key, value):
     return render_menu_items(key, value)
 
+
 @app.callback(Output("tool_widget", "children"),
               Input("tools_menu", "value"))
 def change_tools_menu_item(value):
     if value == "analytics":
         return analitic_tools
+
 
 @app.callback(Output("content_view", "children"),
               Input("content_control", "value"))
@@ -24,15 +27,16 @@ def change_content(value):
     if value == "niche":
         return data_table_niche
 
-@app.callback(
+
+app.clientside_callback(
+    """
+    function(open, close, is_open) {
+        if (open || close) return !is_open;
+        return is_open;
+    }
+    """,
     Output("change_category_modal", "is_open"),
-    [
-        Input("change_category_btn", "n_clicks"),
-        Input("change_category_modal_close", "n_clicks"),
-    ],
-    [State("change_category_modal", "is_open")],
+    Input("change_category_btn", "n_clicks"),
+    Input("change_category_modal_close", "n_clicks"),
+    State("change_category_modal", "is_open"),
 )
-def handle_toggle_change_category_modal(open, close, is_open):
-    if open or close:
-        return not is_open
-    return is_open
